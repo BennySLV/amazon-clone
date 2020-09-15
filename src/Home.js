f735c688533a49ddfb2da28f4d15311eaaf8baaf
Ben Silveston <bsilveston37@gmail.com>
2020-09-15 16:02:11 +0100

    Added sample Amazon home container image.

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Home() {
-	return <div className="home"></div>;
+	return (
+		<div className="home">
+			<div className="home-container">
+				<img
+					src="https://images-eu.ssl-images-amazon.com/images/G/02/kindle/journeys/N2IxYThmYzAt/N2IxYThmYzAt-MjVlNmIyZTUt-w1500._CB407941419_.jpg"
+					alt="Home image"
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default Home;
