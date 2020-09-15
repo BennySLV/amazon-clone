69e48a2288acae3955f407c1a2f042e26e5d4cd3
Ben Silveston <bsilveston37@gmail.com>
2020-09-15 16:12:21 +0100

    Added a class name for the home container image.

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,7 @@ function Home() {
 		<div className="home">
 			<div className="home-container">
 				<img
+					className="home-image"
 					src="https://images-eu.ssl-images-amazon.com/images/G/02/kindle/journeys/N2IxYThmYzAt/N2IxYThmYzAt-MjVlNmIyZTUt-w1500._CB407941419_.jpg"
 					alt="Home image"
 				/>
