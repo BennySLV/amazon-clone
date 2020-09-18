f36f7f8220d9f58f1fb507f5d7a13cf59039a516
Ben Silveston <bsilveston37@gmail.com>
2020-09-18 11:58:50 +0100

    Added image to left-hand column for the checkout page.

--- src/Checkout.js
+++ src/Checkout.js
@@ -2,7 +2,18 @@ import React from "react";
 import "./Checkout.css";
 
 function Checkout() {
-	return <div className="checkout"></div>;
+	return (
+		<div className="checkout">
+			<div className="checkout-left-hand-column">
+				<img
+					className="checkout-ad"
+					src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB4234929668_.jpg"
+					alt=""
+				/>
+			</div>
+			<div className="checkout-right-hand-column"></div>
+		</div>
+	);
 }
 
 export default Checkout;
