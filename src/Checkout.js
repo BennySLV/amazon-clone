6670b53d403ff7380c6088c16dbeaca71dc61461
Ben Silveston <bsilveston37@gmail.com>
2020-09-18 13:36:55 +0100

    Fixed bug - moved "Your shopping basket" title to the left-hand column.

--- src/Checkout.js
+++ src/Checkout.js
@@ -11,9 +11,9 @@ function Checkout() {
 					src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB4234929668_.jpg"
 					alt=""
 				/>
-			</div>
-			<div>
-				<h2 className="checkout-title">Your shopping basket</h2>
+				<div>
+					<h2 className="checkout-title">Your shopping basket</h2>
+				</div>
 			</div>
 			<div className="checkout-right-hand-column">
 				<Subtotal />
