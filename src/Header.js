37a78987c617dd66d33f83d3f82802cdc344ad01
Ben Silveston <bsilveston37@gmail.com>
2020-09-18 12:48:35 +0100

    Added link to checkout page.

--- src/Header.js
+++ src/Header.js
@@ -33,12 +33,14 @@ function Header() {
 					<span className="header-option-line-one">Your</span>
 					<span className="header-option-line-two">Prime</span>
 				</div>
-				<div className="header-option-basket">
-					<ShoppingBasketIcon />
-					<span className="header-option-line-two header-basket-count">
-						0
-					</span>
-				</div>
+				<Link to="/checkout">
+					<div className="header-option-basket">
+						<ShoppingBasketIcon />
+						<span className="header-option-line-two header-basket-count">
+							0
+						</span>
+					</div>
+				</Link>
 			</div>
 		</div>
 	);
