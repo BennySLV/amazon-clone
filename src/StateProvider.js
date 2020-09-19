a241723c7c80e78bdfb548baa2e59a939457156f
Ben Silveston <bsilveston37@gmail.com>
2020-09-19 11:48:20 +0100

    Updated comment.

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -3,7 +3,7 @@ import React, { createContext, useContext, useReducer } from "react";
 // Prepare the data layer
 export const StateContext = createContext();
 
-// Wrap the application and provide the data layer to every app component
+// Provide the data layer to every app component
 export const StateProvider = ({ reducer, initialState, children }) => (
 	<StateContext.Provider value={useReducer(reducer, initialState)}>
 		{children}
