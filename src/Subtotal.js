8c9d27983f880683ca50c96bd7875511eccb85be
Ben Silveston <bsilveston37@gmail.com>
2020-09-20 20:43:23 +0100

    Inserted the render-prop value for the price subtotal value.

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -14,7 +14,7 @@ function Subtotal() {
 					<>
 						<p>
 							Subtotal ({basket?.length} items):
-							<strong> {` ${value}`}</strong>
+							<strong> {value}</strong>
 						</p>
 						<small className="subtotal-gift">
 							<input type="checkbox" /> This order contains a gift
