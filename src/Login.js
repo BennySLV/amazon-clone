7ee7f5d8a68df9f597ecf3b8cf121d3fc74f1b14
Ben Silveston <bsilveston37@gmail.com>
2020-09-23 13:34:42 +0100

    Added the Amazon to the login page.

--- src/Login.js
+++ src/Login.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./Login.css";
 
 function Login() {
-	return <div></div>;
+	return (
+		<div className="login">
+			<img src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
+		</div>
+	);
 }
 
 export default Login;
