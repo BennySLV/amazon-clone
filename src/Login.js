4b47ab0f2f9f8f8ef62250f4e3b458eff59f9b73
Ben Silveston <bsilveston37@gmail.com>
2020-09-23 15:20:59 +0100

    Created the basic form and style for the login page.

--- src/Login.js
+++ src/Login.js
@@ -11,6 +11,23 @@ function Login() {
 					src="https://i.insider.com/539f3ffbecad044276726c01?width=1100&format=jpeg&auto=webp"
 				/>
 			</Link>
+			<div className="login-container">
+				<h1>Sign-in</h1>
+				<form>
+					<h5>Email</h5>
+					<input type="text" />
+
+					<h5>Password</h5>
+					<input type="password" />
+
+					<button> Sign In</button>
+				</form>
+				<p>
+					By signing-in, you agree to the Amazon Clone conditions of
+					Use &amp; Sale. Please see our Privacy Notice, Cookies
+					Notice and our Interest-Based Ads Notice.
+				</p>
+			</div>
 		</div>
 	);
 }
