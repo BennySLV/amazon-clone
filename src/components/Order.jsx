383a93444fd435d86c634f8372d6ea76a95babcb
Ben Silveston <bsilveston37@gmail.com>
2020-10-18 19:13:34 +0100

    Added the "Order" component, which forms the "Orders" component for the application.

---
+++ src/components/Order.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import '../styles/Order.css';
+import moment from 'moment';
+import CheckoutProduct from './CheckoutProduct';
+import CurrencyFormat from 'react-currency-format';
+
+function Order({order}) {
+    return (
+        <div className='order'>
+            <h2>Order(s)</h2>
+            <p>{moment.unix(order.data.created).format("LLLL")}</p>
+            <p className="order-id">
+                <small>{order.id}</small>
+            </p>
+            {order.data.basket?.map(item => (
+                <CheckoutProduct 
+                    id={item.id} 
+                    title={item.title} 
+                    image={item.image} 
+                    price={item.price} 
+                    rating={item.rating}
+                    hideButton
+                />
+            ))}
+            <CurrencyFormat 
+                renderText={(value) => (
+                    <>
+                    <h3 className="order-total">Order total: {value}</h3>
+                    </>
+                )}
+                decimalScale={2}
+                value={order.data.amount / 100}
+                displayType={"text"}
+                thousandSeparator={true}
+                prefix={"£"}
+            />
+        </div>
+    )
+}
+
+export default Order
