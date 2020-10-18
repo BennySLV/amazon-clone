1d5d3ce0d5be6b81291bdec56b08481bba538dc0
Ben Silveston <bsilveston37@gmail.com>
2020-10-18 17:23:54 +0100

    Integrated Cloud Firestore with the payments page.

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -7,6 +7,7 @@ import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import CurrencyFormat from "react-currency-format";
 import { getBasketTotal } from "../reducer";
 import axios from "../axios";
+import {db} from "../firebase";
 
 function Payment() {
 	const [{ basket, user }, dispatch] = useStateValue();
@@ -47,6 +48,14 @@ function Payment() {
 			})
 			.then(({ paymentIntent }) => {
 				// paymentIntent = payment confirmation
+
+				// Push user's payment to the firestore database - (NoSQL data structure syntax):
+				db.collection('users').doc(user?.uid).collection('orders').doc(paymentIntent.id).set({
+					basket: basket,
+					amount: paymentIntent.amount,
+					created: paymentIntent.created
+				})
+
 				setSucceeded(true);
 				setError(null);
 				setProcessing(false);
