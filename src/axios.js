476fd3b0152bf55d11cf6ae33cc6b6c4ce747666
Ben Silveston <bsilveston37@gmail.com>
2020-10-21 14:39:41 +0100

    Deployed the application's back-end into the Cloud Functions API.

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-	baseURL: "http://localhost:5001/clone-79c84/us-central1/api", // The API (cloud function - endpoint) URL
+	baseURL: "https://us-central1-clone-79c84.cloudfunctions.net/api", // The API (cloud function - endpoint) URL
+	//http://localhost:5001/clone-79c84/us-central1/api // localhost api request - for debugging purposes
 });
 
 export default instance;
